1173c3dc066a7554cd7d75a289ce732aeea10b45
Zahary Karadjov <zahary@gmail.com>
2023-01-12 00:46:59 +0200

    Produce a deposit_contract_block_hash.txt file in the CL metadata directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,6 +64,7 @@ gen_cl_config(){
         fi
         /usr/local/bin/eth2-testnet-genesis "${genesis_args[@]}"
         /usr/local/bin/zcli pretty bellatrix BeaconState /data/custom_config_data/genesis.ssz > /data/custom_config_data/parsedBeaconState.json
+        jq -r '.eth1_data.block_hash' /data/custom_config_data/parsedBeaconState.json > /data/custom_config_data/deposit_contract_block_hash.txt
     else
         echo "cl genesis already exists. skipping generation..."
     fi
