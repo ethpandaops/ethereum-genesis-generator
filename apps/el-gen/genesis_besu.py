292db71e02ef86d4270b69c1cad3d1667479ba28
Rafael Matias <rafael@skyle.net>
2023-01-26 16:12:31 +0100

    use cancunTime in besu

--- apps/el-gen/genesis_besu.py
+++ apps/el-gen/genesis_besu.py
@@ -28,7 +28,7 @@ out = {
         "preMergeForkBlock":0,
         "terminalTotalDifficulty":0,
         "shanghaiTime": int(data['genesis_timestamp']) + int(data['genesis_delay']) + (int(data['capella_fork_epoch']) * 32 * 12),
-        "shardingForkTime": int(data['genesis_timestamp']) + int(data['genesis_delay']) + (int(data['deneb_fork_epoch']) * 32 * 12)
+        "cancunTime": int(data['genesis_timestamp']) + int(data['genesis_delay']) + (int(data['deneb_fork_epoch']) * 32 * 12)
     },
     "alloc": {
         # Allocate 1 wei to all possible pre-compiles.
