39f70bf0f4a842c40d431c949af6bb89dc240d1a
Rafael Matias <rafael@skyle.net>
2023-04-23 12:00:25 +0200

    fixup genesis.json fork times

--- apps/el-gen/genesis_geth.py
+++ apps/el-gen/genesis_geth.py
@@ -44,8 +44,8 @@ else:
             "londonBlock":0,
             "mergeForkBlock":0,
             "terminalTotalDifficulty":0,
-            "shanghaiTime": 0,
-            "shardingForkTime":  int(data['genesis_timestamp']) + int(data['genesis_delay']) + (int(data['deneb_fork_epoch']) * 32 * 12)
+            "shanghaiTime": int(data['genesis_timestamp']) + int(data['genesis_delay']) + (int(data['capella_fork_epoch']) * 32 * 12),
+            "cancunTime":  int(data['genesis_timestamp']) + int(data['genesis_delay']) + (int(data['deneb_fork_epoch']) * 32 * 12)
         },
         "alloc": {
             # Allocate 1 wei to all possible pre-compiles.
