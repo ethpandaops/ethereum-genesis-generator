f8d99ac1dc1906e9a3ce9adbc2bafe1dfa57c5d8
pk910 <philipp@pk910.de>
2024-03-06 01:10:37 +0100

    remove nonce from 4788 genesis deployment

--- apps/el-gen/genesis_chainspec.py
+++ apps/el-gen/genesis_chainspec.py
@@ -144,7 +144,6 @@ else:
             },
             "0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02": {
                 "balance": "0",
-                "nonce": "1",
                 "code": "0x3373fffffffffffffffffffffffffffffffffffffffe14604d57602036146024575f5ffd5b5f35801560495762001fff810690815414603c575f5ffd5b62001fff01545f5260205ff35b5f5ffd5b62001fff42064281555f359062001fff015500"
             }
         },
