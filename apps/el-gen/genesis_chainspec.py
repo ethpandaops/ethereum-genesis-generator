13428cdf98a62260ba2edc59d1e72028c61dc2ba
Barnabas Busa <busa.barnabas@gmail.com>
2024-05-09 08:32:08 +0200

    fix: add nethermind prague flags

--- apps/el-gen/genesis_chainspec.py
+++ apps/el-gen/genesis_chainspec.py
@@ -204,7 +204,32 @@ else:
         out["accounts"][addr] = alloc_entry
 
 if 'electra_fork_epoch' in data:
-    out['params']['eip6800TransitionTimestamp']= hex(
+    out['params']['eip6110TransitionTimestamp']= hex(
+        int(data['genesis_timestamp']) +
+        int(data['genesis_delay']) +
+        int(data['electra_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
+    )
+    out['params']['eip7002TransitionTimestamp']= hex(
+        int(data['genesis_timestamp']) +
+        int(data['genesis_delay']) +
+        int(data['electra_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
+    )
+    out['params']['eip2537TransitionTimestamp']= hex(
+        int(data['genesis_timestamp']) +
+        int(data['genesis_delay']) +
+        int(data['electra_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
+    )
+    out['params']['eip2935TransitionTimestamp']= hex(
+        int(data['genesis_timestamp']) +
+        int(data['genesis_delay']) +
+        int(data['electra_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
+    )
+    out['params']['eip3074TransitionTimestamp']= hex(
+        int(data['genesis_timestamp']) +
+        int(data['genesis_delay']) +
+        int(data['electra_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
+    )
+    out['params']['eip7685TransitionTimestamp']= hex(
         int(data['genesis_timestamp']) +
         int(data['genesis_delay']) +
         int(data['electra_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
