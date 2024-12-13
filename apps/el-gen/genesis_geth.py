1696e8f1af469979eaba47c6f35882e5ec30fc34
Barnabas Busa <busa.barnabas@gmail.com>
2024-12-13 16:01:20 +0100

    feat: add 7840 - custom blob max/target for el

--- apps/el-gen/genesis_geth.py
+++ apps/el-gen/genesis_geth.py
@@ -198,7 +198,7 @@ if 'capella_fork_epoch' in data and not isNamedTestnet:
             int(data['capella_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
     else:
         out['config']['shanghaiTime'] = 0
-        
+
 
 if 'deneb_fork_epoch' in data and not isNamedTestnet:
     if data['deneb_fork_epoch'] != 0:
@@ -215,15 +215,27 @@ if 'electra_fork_epoch' in data:
             int(data['genesis_timestamp']) + \
             int(data['genesis_delay']) + \
             int(data['electra_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
+        out['config']['blobSchedule'] = {
+            "prague": {
+                "target": data['target_blobs_per_block_prague'],
+                "max": data['max_blobs_per_block_prague']
+            }
+        }
     else:
         out['config']['pragueTime'] = 0
 
 if 'fulu_fork_epoch' in data:
-    if data['fulu_fork_epoch'] != 0:   
+    if data['fulu_fork_epoch'] != 0:
         out['config']['osakaTime'] =  \
             int(data['genesis_timestamp']) + \
             int(data['genesis_delay']) + \
             int(data['fulu_fork_epoch']) * ( 32 if data['preset_base']=='mainnet' else 8 ) * int(data['slot_duration_in_seconds'])
+        out['config']['blobSchedule'] = {
+            "osaka": {
+                "target": data['target_blobs_per_block_osaka'],
+                "max": data['max_blobs_per_block_osaka']
+            }
+        }
     else:
         out['config']['osakaTime'] = 0
 
