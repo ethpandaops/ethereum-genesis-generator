94fefb67b24f7d4c1d37582c739e136cf3adda3d
pk910 <philipp@pk910.de>
2025-01-15 22:59:27 +0100

    fix ephemery genesis

--- apps/el-gen/genesis_besu.py
+++ apps/el-gen/genesis_besu.py
@@ -108,7 +108,7 @@ else:
         },
         "coinbase": "0x0000000000000000000000000000000000000000",
         "baseFeePerGas": "0x3B9ACA00",
-        "difficulty": "0x0",
+        "difficulty": "0x01",
         "extraData": "",
         "gasLimit": hex(int(data['genesis_gaslimit'] if 'genesis_gaslimit' in data and data['genesis_gaslimit'] is not None else 25000000)),
         "nonce": "0x1234",
--- apps/el-gen/genesis_geth.py
+++ apps/el-gen/genesis_geth.py
@@ -106,7 +106,7 @@ else:
             }
         },
         "coinbase": "0x0000000000000000000000000000000000000000",
-        "difficulty": "0x0",
+        "difficulty": "0x01",
         "extraData": "",
         "gasLimit": hex(int(data['genesis_gaslimit'] if 'genesis_gaslimit' in data and data['genesis_gaslimit'] is not None else 25000000)),
         "nonce": "0x1234",
