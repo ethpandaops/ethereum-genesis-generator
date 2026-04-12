27005c50ae24343c6b529b78a0c980f3630938f4
pk910 <philipp@pk910.de>
2026-04-12 04:31:44 +0200

    add MAX_REQUEST_DATA_COLUMN_SIDECARS

--- .github/check-consensus-spec-values.py
+++ .github/check-consensus-spec-values.py
@@ -53,6 +53,7 @@ EXCLUDED_FIELDS = {
     # Deprecated fields removed from spec configs, kept for backwards compatibility with latest client releases
     "SECONDS_PER_SLOT",
     "MAX_REQUEST_BLOB_SIDECARS_ELECTRA",
+    "MAX_REQUEST_DATA_COLUMN_SIDECARS",
 }
 
 
