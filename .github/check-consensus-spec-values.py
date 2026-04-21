47a3c0538def54f755c27f804f1fbbe65315fb4c
pk910 <philipp@pk910.de>
2026-04-21 10:48:44 +0200

    remove from ignore

--- .github/check-consensus-spec-values.py
+++ .github/check-consensus-spec-values.py
@@ -52,8 +52,6 @@ EXCLUDED_FIELDS = {
     "BLOB_SCHEDULE",
     # Deprecated fields removed from spec configs, kept for backwards compatibility with latest client releases
     "SECONDS_PER_SLOT",
-    "MAX_REQUEST_BLOB_SIDECARS_ELECTRA",
-    "MAX_REQUEST_DATA_COLUMN_SIDECARS",
 }
 
 
